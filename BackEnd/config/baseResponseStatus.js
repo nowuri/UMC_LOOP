06a0d5ec3545cd88f772739cb015e4f4bb8a28df
KwonPodo <ludin9901@gmail.com>
2023-01-20 16:00:52 +0900

    아이디 중복체크 API 작성

--- BackEnd/config/baseResponseStatus.js
+++ BackEnd/config/baseResponseStatus.js
@@ -35,7 +35,7 @@ module.exports = {
 
     // Response error
     SIGNUP_REDUNDANT_EMAIL : { "isSuccess": false, "code": 3001, "message":"중복된 이메일입니다." },
-    SIGNUP_REDUNDANT_NICKNAME : { "isSuccess": false, "code": 3002, "message":"중복된 닉네임입니다." },
+    SIGNUP_REDUNDANT_USERID : { "isSuccess": false, "code": 3002, "message":"중복된 아이디입니다." },
 
     SIGNIN_EMAIL_WRONG : { "isSuccess": false, "code": 3003, "message": "아이디가 잘못 되었습니다." },
     SIGNIN_PASSWORD_WRONG : { "isSuccess": false, "code": 3004, "message": "비밀번호가 잘못 되었습니다." },
