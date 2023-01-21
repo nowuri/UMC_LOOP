67a4e0099214a01fcf233896028281cf8ec2f60c
nowuri <109161102+nowuri@users.noreply.github.com>
2023-01-21 22:21:03 +0900

    [프론트] QnA 최종

--- app/src/main/java/com/example/qna/Question.kt
+++ app/src/main/java/com/example/qna/Question.kt
@@ -1,14 +1,22 @@
 package com.example.qna
 
+import android.app.Dialog
 import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.core.graphics.drawable.DrawableCompat.inflate
 import com.example.interested.R
 import com.example.interested.SignUp2Activity
 import com.example.interested.databinding.ActivityQuestionBinding
 import com.example.search.Search
+import com.jakewharton.threetenabp.AndroidThreeTen
+import java.time.format.DateTimeFormatter
+import org.threeten.bp.LocalDate
+import java.text.SimpleDateFormat
 
 class Question : AppCompatActivity() {
     private lateinit var viewBinding: ActivityQuestionBinding
@@ -17,10 +25,20 @@ class Question : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(viewBinding.root)
 
+        val currentTime : Long = System.currentTimeMillis()
+        val dataFormat = SimpleDateFormat("HH")
+
         viewBinding.kakao.setOnClickListener(){
             val address = "https://www.youthcenter.go.kr/kakao/kakao.html"
-            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(address))
-            startActivity(intent)
+
+            if(dataFormat.format(currentTime).toInt() >= 8 && dataFormat.format(currentTime).toInt()<=20 ) {
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(address))
+                startActivity(intent)
+            }
+            else{
+                val dlg = Dialog1(this)
+                dlg.myDig()
+            }
         }
 
         viewBinding.operator.setOnClickListener(){
@@ -32,8 +50,14 @@ class Question : AppCompatActivity() {
         viewBinding.call.setOnClickListener(){
             val intent = Intent(Intent.ACTION_DIAL)
             intent.data = Uri.parse("tel:18119876")
-            if(intent.resolveActivity(packageManager) != null){
-                startActivity(intent)
+            if(dataFormat.format(currentTime).toInt() >= 9 && dataFormat.format(currentTime).toInt()<=18 ) {
+                if(intent.resolveActivity(packageManager) != null){
+                    startActivity(intent)
+                }
+            }
+            else{
+                val dlg = Dialog1(this)
+                dlg.myDig()
             }
         }
 
