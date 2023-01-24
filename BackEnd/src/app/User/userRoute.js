f38281d89243892c42446aac77cfbf10d1af391b
minyeong <ninja2319@naver.com>
2023-01-24 16:30:15 +0900

    gitignore 삭제

--- BackEnd/src/app/User/userRoute.js
+++ BackEnd/src/app/User/userRoute.js
@@ -7,7 +7,7 @@ module.exports = function(app){
    const user = require('./userController');
    const jwtMiddleware = require('../../../config/jwtMiddleware');
 
-   app.post('/app/users/oAuth/kakao-login', user.kakaoLogin);
+   app.post('/app/users/oAkakao-login', user.kakaoLogin);
 
    app.get('/app/users/oAuth/kakao', passport.authenticate('kakao-login'));
 
@@ -20,45 +20,14 @@ module.exports = function(app){
    },)
 
 
-module.exports = function(app) {
-  const user = require('./userController');
-  const jwtMiddleware = require('../../../config/jwtMiddleware');
+   // 0. 테스트 API
+   // app.get('/app/test', user.getTest)
 
+   //  // 1. 유저 생성 (회원가입) API
+   //  app.post('/app/users', user.postUsers);
 
-  // 자체 작업 - 준/권준형
-  // ----------------------------------------------
-  // 0. 테스트 API
-  app.get('/app/test', user.getTest)
-
-  // 닉제임 중복 확인 API
-  app.get('/app/users/api/:userId', user.checkOverlappingUser);
-
-  // SMS 전송 API
-  app.post('/app/users/api/tokens', user.sendTokenToSMS);
-
-  // UMC Template
-  // ----------------------------------------------
-  // 0. 테스트 API
-  // app.get('/app/test', user.getTest)
-
-  // 1. 유저 생성 (일반 회원가입) API
-  // app.post('/app/users', user.postUsers);
-
-
-  // 2. 유저 조회 API (+ 검색)
-  // app.get('/app/users', user.getUsers);
-
-  // 3. 특정 유저 조회 API
-  // app.get('/app/users/:userId', user.getUserById);
-
-
-  // TODO: After 로그인 인증 방법 (JWT)
-  // 로그인 하기 API (JWT 생성)
-  // app.post('/app/login', user.login);
-
-  // 회원 정보 수정 API (JWT 검증 및 Validation - 메소드 체이닝 방식으로 jwtMiddleware 사용)
-  // app.patch('/app/users/:userId', jwtMiddleware, user.patchUsers)
-
+   //  // 2. 유저 조회 API (+ 검색)
+   //  app.get('/app/users',user.getUsers); 
 
    //  // 3. 특정 유저 조회 API
    //  app.get('/app/users/:userId', user.getUserById);
