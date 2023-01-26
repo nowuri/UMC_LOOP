2639f8fd99a16915b5fe48fa7dde0d31b5e3f4ed
nowuri <109161102+nowuri@users.noreply.github.com>
2023-01-27 01:39:16 +0900

    [프론트] 아이디, 비밀번호 찾기 화면 이동 추가

--- app/src/main/java/com/example/find/ResetPW.kt
+++ app/src/main/java/com/example/find/ResetPW.kt
@@ -2,11 +2,13 @@ package com.example.find
 
 import android.app.Dialog
 import android.content.DialogInterface
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
+import com.example.home.Home
 import com.example.interested.databinding.ActivityResetPwBinding
 
 class ResetPW : AppCompatActivity() {
@@ -28,7 +30,10 @@ class ResetPW : AppCompatActivity() {
                 builder.setTitle("비밀번호 재설정")
                     .setMessage("비밀번호가 재설정되었습니다.")
                     .setPositiveButton("확인",
-                        DialogInterface.OnClickListener{ dialog, id -> }
+                        DialogInterface.OnClickListener{ dialog, id ->
+                            val intent = Intent(this,Home::class.java)
+                            startActivity(intent)
+                        }
                     )
                 builder.show()
             }
