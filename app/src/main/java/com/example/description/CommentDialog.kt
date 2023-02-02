165d313c11c2d5198a8bd8d61fd475b7aa59ea5b
yeni-choi <yewonchoi@ewhain.net>
2023-02-02 23:35:06 +0900

    [프론트] 정책 추천/비추천 코멘트 다이얼로그->액티비티 데이터 넘기기
    +) 로그를 통해 받아온 값을 확인할 수 있도록 했습니다.

--- app/src/main/java/com/example/description/CommentDialog.kt
+++ app/src/main/java/com/example/description/CommentDialog.kt
@@ -1,16 +1,21 @@
 package com.example.description
 import android.app.Dialog
+import android.content.Intent
+import android.os.Bundle
 import android.view.Window
 import androidx.appcompat.app.AppCompatActivity
-import com.example.interested.databinding.ActivityCommentDialogBinding
+import com.example.interested.SignUp2Activity
+import com.example.interested.databinding.DialogCommentBinding
+import com.example.interested.databinding.SignupPt2Binding
+import com.example.signup.SignUp1Activity
 
 class CommentDialog(private val context : AppCompatActivity) {
 
-    private lateinit var binding : ActivityCommentDialogBinding
+    private lateinit var binding : DialogCommentBinding
     private val dlg = Dialog(context)   //부모 액티비티의 context 가 들어감
 
     fun show() {
-        binding = ActivityCommentDialogBinding.inflate(context.layoutInflater)
+        binding = DialogCommentBinding.inflate(context.layoutInflater)
 
         dlg.requestWindowFeature(Window.FEATURE_NO_TITLE)   //타이틀바 제거
         dlg.setContentView(binding.root)     //다이얼로그에 사용할 xml 파일을 불러옴
@@ -19,6 +24,7 @@ class CommentDialog(private val context : AppCompatActivity) {
         //ok 버튼 동작
         binding.btnFinish.setOnClickListener {
             //작성 완료
+            onClickedListener.onClicked(binding.inputComment.text.toString())
             dlg.dismiss()
         }
 
@@ -29,4 +35,15 @@ class CommentDialog(private val context : AppCompatActivity) {
 
         dlg.show()
     }
+
+    interface ButtonClickListener {
+        fun onClicked(myName: String)
+    }
+
+    private lateinit var onClickedListener: ButtonClickListener
+
+    fun setOnClickedListener(listener: ButtonClickListener) {
+        onClickedListener = listener
+    }
+
 }
