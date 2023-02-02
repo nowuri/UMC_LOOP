f85cf87a1b67b770281a874f18588c015fff7d52
KwonPodo <ludin9901@gmail.com>
2023-02-02 18:08:19 +0900

    [백엔드] 정부정책 API쪽 regex-email 제거

--- BackEnd/src/app/Policy/policyController.js
+++ BackEnd/src/app/Policy/policyController.js
@@ -1,7 +1,5 @@
 const { response, errResponse } = require('../../../config/response.js');
 const baseResponseStatus = require('../../../config/baseResponseStatus.js');
-const baseResponse = require("../../../config/baseResponseStatus");
-const regexEmail = require("regex-email");
 const userProvider = require("../User/userProvider");
 const axios = require('axios').default;
 const fs = require('fs');
@@ -14,35 +12,37 @@ const xml2js = require('xml2js');
 
 
 //필드에 리퀘스트 받는것 구현하기 지역별? 정책별?
-exports.getPolicies = async function (req, res) {
+exports.getPolicies = async function(req, res) {
 
-    // request 에서 원하는 필터 받아오기(지역, 정책)
-    let fields = req.param('fields'); // bizTycdSel (지역)
-    let region = req.param('region'); // srchPolyBizSecd (정책)
+  // request 에서 원하는 필터 받아오기(지역, 정책)
+  const fields = req.params.fields; // bizTycdSel (지역)
+  const region = req.params.region; // srchPolyBizSecd (정책)
+  // let fields = req.param('fields'); // bizTycdSel (지역)
+  // let region = req.param('region'); // srchPolyBizSecd (정책)
 
-    const config = {
-        method: 'get',
-        url: 'https://www.youthcenter.go.kr/opi/empList.do',
-        params: {
-            openApiVlak: '2809748344615158ecb64134',
-            bizTycdSel: fields, //정책 id
-            srchPolyBizSecd: region, //지역 id
-            display:10,
-            pageIndex: 1
-        },
-    }
+  const config = {
+    method: 'get',
+    url: 'https://www.youthcenter.go.kr/opi/empList.do',
+    params: {
+      openApiVlak: '2809748344615158ecb64134',
+      bizTycdSel: fields, //정책 id
+      srchPolyBizSecd: region, //지역 id
+      display: 10,
+      pageIndex: 1
+    },
+  }
 
-    axios(config).then((policyResponse) => {
-        xml2js.parseString(policyResponse.data, (err, data) => {
-            if (err) {
-                console.error(err);
-                res.send(errResponse(baseResponseStatus.SIGNUP_POLICY_ERROR_TYPE))
-            } else {
-                console.dir(data.empsInfo.emp.length, { depth: null });
-                res.send(response(baseResponseStatus.SUCCESS, data.empsInfo.emp))
-            }
-        });
-    }).catch((error) => {
-        res.send(response(baseResponseStatus.POLICY_CALL_ERROR_TYPE));
-    })
+  axios(config).then((policyResponse) => {
+    xml2js.parseString(policyResponse.data, (err, data) => {
+      if (err) {
+        console.error(err);
+        res.send(errResponse(baseResponseStatus.SIGNUP_POLICY_ERROR_TYPE))
+      } else {
+        console.dir(data.empsInfo.emp.length, { depth: null });
+        res.send(response(baseResponseStatus.SUCCESS, data.empsInfo.emp))
+      }
+    });
+  }).catch((error) => {
+    res.send(response(baseResponseStatus.POLICY_CALL_ERROR_TYPE));
+  })
 };
