45554bc03bedccde421c8dbec45de1e88eea68c9
yeni-choi <yewonchoi@ewhain.net>
2023-03-20 23:08:29 +0900

    [프론트] 홈화면 5개 지역 정책 출력 세팅 완료

--- app/src/main/java/com/example/home/HomeGang.kt
+++ app/src/main/java/com/example/home/HomeGang.kt
@@ -2,6 +2,7 @@ package com.example.home
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -11,6 +12,10 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.interested.R
 import com.example.interested.databinding.FragmentHomeGangBinding
 import com.example.interested.databinding.FragmentHomeGyeonggiBinding
+import com.example.network.RetrofitClient
+import com.google.gson.JsonObject
+import retrofit2.Call
+import retrofit2.Response
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -34,13 +39,7 @@ class HomeGang : Fragment() {
 
         mainActivity = context as Home
     }
-    var dataList: ArrayList<Homedata> = arrayListOf(
-        Homedata("정책이름","부서이름"),
-        Homedata("정책이름","부서이름"),
-        Homedata("정책이름","부서이름"),
-        Homedata("정책이름","부서이름"),
-        Homedata("정책이름","부서이름")
-    )
+    var dataList: ArrayList<Homedata> = arrayListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -48,6 +47,9 @@ class HomeGang : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+
+        val region = "강원경상"
+        RetrofitWork(region).work()
     }
 
     override fun onCreateView(
@@ -58,12 +60,63 @@ class HomeGang : Fragment() {
         viewbinding = FragmentHomeGangBinding.inflate(layoutInflater)
         //setOnClickListener()
 
-        var list: ArrayList<Homedata> = dataList
-        val DataRVAdapter = HomeAdapter4(mainActivity,list)
-        viewbinding.rvRefrigerator.layoutManager = LinearLayoutManager(activity, RecyclerView.VERTICAL,false)
-        viewbinding.rvRefrigerator.adapter = DataRVAdapter
+        val DataRVAdapter = HomeAdapter4(mainActivity, dataList)
 
         return viewbinding.root
+    }
+
+
+    inner class RetrofitWork(private val region: String){
+        fun work(){
+            Log.e("정책 불러오기","시작")
+            val service = RetrofitClient.emgMedService
+
+            service.HomeDataGet(region)
+                .enqueue(object : retrofit2.Callback<JsonObject>{
+                    override fun onResponse(
+                        call: Call<JsonObject>,
+                        response: Response<JsonObject>,
+                    ) {
+                        if(response.isSuccessful()) {
+                            val responsebody = response.body().toString()
+                            Log.e("정책 불러오기 성공","$responsebody")
+
+                            //2.정책 result array 형식으로 뽑아옴
+                            val jsonArray = response.body()?.getAsJsonArray("result")
+                            Log.e("정책 array:","$jsonArray")
+
+                            //3. 정책 array에서 정책 다섯개 뽑아옴
+                            if (jsonArray != null) {
+                                var a: Int = 0
+                                while(a <= 4){
+                                    val Jsonfor = jsonArray[a].getAsJsonObject()
+                                    val policyname = Jsonfor.get("policyName").getAsString()
+                                    val department = Jsonfor.get("department").getAsString()
+                                    dataList.add(
+                                        Homedata(
+                                            policyname,
+                                            department
+                                        )
+                                    )
+                                    a+=1
+                                }
+                                val DataRVAdapter = HomeAdapter4(mainActivity, dataList)
+                                viewbinding.rvRefrigerator.layoutManager = LinearLayoutManager(activity, RecyclerView.VERTICAL,false)
+                                viewbinding.rvRefrigerator.adapter = DataRVAdapter
+
+                            }
+                        }
+                        else {
+                            val code = response.code()
+                        }
+                    }
+                    override fun onFailure(call: Call<JsonObject>, t: Throwable) {
+                        Log.e("정책 불러오기 실패",t.message.toString())
+                    }
+                })
+        }
+
+
     }
 
     companion object {
